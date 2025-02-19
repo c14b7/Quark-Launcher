cf4250ea4b3d2fbcd6e45f1404427b108a28f325
C14B7 <hubert.kula@email.com>
2025-02-19 18:18:09 +0100

    s

--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import { rightSidebarStyle, userProfileStyle, additionalContentStyle, friendsListStyle } from '../styles';
 
 function RightSidebar() {
   return (
-    <div id="right-sidebar">
-      <div id="user-profile">
+    <div id="right-sidebar" css={rightSidebarStyle}>
+      <div id="user-profile" css={userProfileStyle}>
         <div>Username</div>
         <div>User ID</div>
       </div>
-      <div id="additional-content"></div>
-      <div id="friends-list">
+      <div id="additional-content" css={additionalContentStyle}></div>
+      <div id="friends-list" css={friendsListStyle}>
         <div>Friends:</div>
         {/* List of friends will be dynamically added here */}
       </div>
--- src/styles.js
+++ src/styles.js
@@ -1,106 +1,103 @@
-#root {
+import { css } from '@emotion/react';
+
+export const rootStyle = css`
   display: flex;
   height: 100vh;
   background-color: #1e1e1e;
   color: #ffffff;
-}
+`;
 
-#left-sidebar {
+export const leftSidebarStyle = css`
   width: 20%;
   background-color: #2e2e2e;
   padding: 10px;
   display: flex;
   flex-direction: column;
-}
+`;
 
-#search-bar {
+export const searchBarStyle = css`
   padding: 10px;
   background-color: #3e3e3e;
   margin-bottom: 10px;
   color: #ffffff;
-}
+`;
 
-#menu-items {
+export const menuItemsStyle = css`
   flex-grow: 1;
-}
-#menu-items div {
+`;
+
+export const menuItemStyle = css`
   padding: 10px;
   margin-bottom: 5px;
   background-color: #3e3e3e;
-}
+`;
 
-#game-list {
+export const gameListStyle = css`
   margin-top: 10px;
-}
+`;
 
-#main-content {
+export const mainContentStyle = css`
   width: 60%;
   padding: 10px;
   display: flex;
   flex-direction: column;
-}
+`;
 
-.large-section {
+export const largeSectionStyle = css`
   height: 100px;
   background-color: #3e3e3e;
   margin-bottom: 10px;
-}
+`;
 
-.small-section-row {
+export const smallSectionRowStyle = css`
   display: flex;
   justify-content: space-between;
   margin-bottom: 10px;
-}
+`;
 
-.small-section {
+export const smallSectionStyle = css`
   width: 15%;
   height: 50px;
   background-color: #3e3e3e;
-}
+`;
 
-#right-sidebar {
+export const rightSidebarStyle = css`
   width: 20%;
   background-color: #2e2e2e;
   padding: 10px;
   display: flex;
   flex-direction: column;
-}
+`;
 
-#user-profile {
+export const userProfileStyle = css`
   padding: 10px;
   background-color: #3e3e3e;
   margin-bottom: 10px;
-}
+`;
 
-#additional-content {
+export const additionalContentStyle = css`
   flex-grow: 1;
   background-color: #3e3e3e;
   margin-bottom: 10px;
-}
+`;
 
-#friends-list {
+export const friendsListStyle = css`
   margin-top: 10px;
-}
+`;
 
-#bottom-left-buttons {
+export const bottomLeftButtonsStyle = css`
   position: absolute;
   bottom: 10px;
   left: 10px;
   display: flex;
   flex-direction: column;
-}
+`;
 
-#bottom-left-buttons button {
+export const bottomLeftButtonStyle = css`
   margin-bottom: 5px;
   padding: 10px;
   background-color: #3e3e3e;
   border: none;
   color: #ffffff;
   cursor: pointer;
-}
-
-#app-root {
-  display: flex;
-  height: 100vh;
-  width: 100%;
-}
+`;
